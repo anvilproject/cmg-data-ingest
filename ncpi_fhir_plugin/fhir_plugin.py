b90b077a038dbb399839b148b1616f049a42fd72
Eric Torstenson <eric.s.torstenson@vumc.org>
2020-12-04 13:25:36 -0600

    Added support for variants

--- ncpi_fhir_plugin/fhir_plugin.py
+++ ncpi_fhir_plugin/fhir_plugin.py
@@ -29,8 +29,13 @@ from ncpi_fhir_plugin.target_api_builders.hpo_observation import HumanPhenotype
 from ncpi_fhir_plugin.target_api_builders.tissue_affected_status import TissueAffectedStatus 
 from ncpi_fhir_plugin.target_api_builders.sequencing_center import SequencingCenter
 from ncpi_fhir_plugin.target_api_builders.sequencing_file import SequencingFile
+from ncpi_fhir_plugin.target_api_builders.sequencing_file_no_drs import SequencingFileNoDrs
 from ncpi_fhir_plugin.target_api_builders.sequencing_task import SequencingTask 
 from ncpi_fhir_plugin.target_api_builders.sequencing_file_info import SequencingFileInfo
+from ncpi_fhir_plugin.target_api_builders.discovery_variant import DiscoveryVariant
+from ncpi_fhir_plugin.target_api_builders.discovery_implication import DiscoveryImplication 
+from ncpi_fhir_plugin.target_api_builders.discovery_report import DiscoveryReport 
+
 from ncpi_fhir_plugin import common
 
 from fhir_walk.fhir_host import FhirHost
@@ -48,8 +53,12 @@ all_targets = [
     HumanPhenotype,
     TissueAffectedStatus,
     SequencingFile,
+    SequencingFileNoDrs,
     SequencingTask,
-    SequencingFileInfo 
+    SequencingFileInfo,
+    DiscoveryVariant,
+    DiscoveryImplication,
+    DiscoveryReport
 ]
 #print(inspect.getsource(Patient.build_entity))
 clients = {}
