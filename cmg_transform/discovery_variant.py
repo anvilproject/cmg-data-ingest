858e410681629fdd43b53aee33127484f9a0dd49
Eric Torstenson <eric.s.torstenson@vumc.org>
2021-01-15 09:31:10 -0600

    Redirect all transformations to a single point for logging

--- cmg_transform/discovery_variant.py
+++ cmg_transform/discovery_variant.py
@@ -12,48 +12,58 @@ class DiscoveryVariant:
         'GRCh38DH': 'GRCh38'
     }
     def __init__(self, row):
+        if "subject_id" not in row:
+            print(sorted(row.keys()))
         self.id = Transform.CleanSubjectId(row['subject_id']) # Transform.CleanSubjectId(row['subject_id'])
         self.sample_id = row['sample_id']
-        self.gene = row['gene']
-        self.gene_class = row['gene_class']
+        self.gene = Transform.ExtractVar(row, 'gene')
+        self.gene_class = Transform.ExtractVar(row, 'gene_class')
         self.gene_code = self.gene
-        self.zygosity = row['zygosity']
+        self.zygosity = Transform.ExtractVar(row, 'zygosity')
 
         # Some of the genes are done weirdly, so we'll try and fix them now:
-        if ":" in self.gene:
+        if self.gene is not None and ":" in self.gene:
             self.gene, tier = self.gene.split(":")
             if self.gene_class == "Candidate":
                 self.gene_class = f"{tier} - Candidate"
 
         if 'variant_genome_build' in row:
-            self.ref_seq = row['variant_genome_build']
+            self.ref_seq = Transform.ExtractVar(row, 'variant_genome_build', constants.DISCOVERY.VARIANT.GENOME_BUILD, default_to_empty=True)
         else:
-            self.ref_seq = DiscoveryVariant.genome_builds[Sequencing.genome_builds[self.sample_id]]
-        self.chrom = Transform.CleanVar(constants.DISCOVERY.VARIANT.CHROMOSOME, row['chrom'])
-        self.pos = row['pos']
-        self.ref = row['ref']
-        self.alt = row['alt']
-        self.hgvsc = row['hgvsc']
-        self.hgvsp = row['hgvsp']
-        self.transcript = row['transcript']
-        self.sv_name = row['sv_name']
-        self.sv_type = row['sv_type']
-        self.significance = row['significance']
+            if self.sample_id in Sequencing.genome_builds:
+                self.ref_seq = DiscoveryVariant.genome_builds[Sequencing.genome_builds[self.sample_id]]
+            else:
+                self.ref_seq = None
+        self.chrom = Transform.ExtractVar(row, 'chrom', constants.DISCOVERY.VARIANT.CHROMOSOME, default_to_empty=True)
+        self.pos = Transform.ExtractVar(row, 'pos')
+        self.ref = Transform.ExtractVar(row, 'ref')
+        self.alt = Transform.ExtractVar(row, 'alt')
+        self.hgvsc = Transform.ExtractVar(row, 'hgvsc')
+        self.hgvsp = Transform.ExtractVar(row, 'hgvsp')
+        self.transcript = Transform.ExtractVar(row, 'transcript')
+        self.sv_name = Transform.ExtractVar(row, 'sv_name')
+        self.sv_type = Transform.ExtractVar(row, 'sv_type')
+        self.significance = Transform.ExtractVar(row, 'significance', constants.DISCOVERY.VARIANT.SIGNIFICANCE)
         self.variant_id = f"{self.chrom}|{self.pos}|{self.ref}|{self.alt}"
 
         if self.gene:
-            gene = Gene.get_gene(self.gene)
-            if gene:
-                self.gene_code = gene.id
+            try:
+                gene = Gene.get_gene(self.gene)
+                if gene:
+                    self.gene_code = gene.id
+            except:
+                print(f"There was an issue pulling variant details data down for, {self.gene}")
 
         # if we do get a variant, the ID can be used to construct a URL for clinvar entry, which 
         # may be quite informative
         if self.variant_id is None:
             print(f"Get Variant returned nothing: {self.hgvsc} : {self.hgvsp} : {self.transcript}")
             sys.exit(1)
-        self.variant = get_variant(self.hgvsc, self.transcript)
 
-        self.inheritance = row['inheritance_description']
+        if self.hgvsc is not None and self.transcript is not None:
+            self.variant = get_variant(self.hgvsc, self.transcript)
+
+        self.inheritance = Transform.ExtractVar(row, 'inheritance_description')
 
     def add_variant_ids(self, variant_lkup):
         variant_lkup[self.id].append(f"{self.variant_id}+{self.sample_id}")
