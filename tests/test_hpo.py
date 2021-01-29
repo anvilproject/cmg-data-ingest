21c319dcd5f19006b3b41522c9964ef95c5d5376
Eric Torstenson <eric.s.torstenson@vumc.org>
2021-01-29 15:12:09 -0600

    Add more descriptive failure messages

--- tests/test_hpo.py
+++ tests/test_hpo.py
@@ -15,23 +15,25 @@ def test_disease_details(study, transformed_dir):
 
     patients = study.Patients()
 
+    line_number = 0
     with hpo_file.open() as f:
         reader = DictReader(f, delimiter='\t')
 
         for line in reader:
+            line_number += 1
             subject_id = line[CONCEPT.PARTICIPANT.ID]
             phenotype_id = line[CONCEPT.PHENOTYPE.HPO_ID]
             phenotype_name = line[CONCEPT.PHENOTYPE.NAME]
             phenotype_observed = line[CONCEPT.PHENOTYPE.OBSERVED]
 
-            assert subject_id in patients, f"Did we get the subject ({subject_id}) from the server?"
+            assert subject_id in patients, f"Did we get the subject ({subject_id}) @ #{line_number} from the server? {len(patients)} patients returned"
             subject = patients[subject_id]
             pheno_present, pheno_absent = subject.phenotypes()
 
             if phenotype_observed == constants.PHENOTYPE.OBSERVED.PRESENT:
-                assert phenotype_id in pheno_present, "Make sure that the phenotype code is present (in present)"
+                assert phenotype_id in pheno_present, f"Make sure that the phenotype code is present (in present) for {subject_id} @ {line_number}"
                 assert phenotype_name == pheno_present[phenotype_id].name 
             else:
-                assert phenotype_id in pheno_absent, "Make sure that the phenotype code is present (in present)"
+                assert phenotype_id in pheno_absent, f"Make sure that the phenotype code '{phenotype_id}' is present (in absent) for {subject_id} | '{phenotype_observed}' @ {line_number}"
                 assert phenotype_name == pheno_absent[phenotype_id].name 
 
