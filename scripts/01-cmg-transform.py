384cd3f5775c0fa84891c12d4a315004822c7cfa
Eric Torstenson <eric.s.torstenson@vumc.org>
2021-10-29 09:51:42 -0500

    Update to current model that includes consent and groups

--- scripts/01-cmg-transform.py
+++ scripts/01-cmg-transform.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python
 
 import csv
-from fhir_walk.config import DataConfig 
+from os import getenv
+
+from yaml import safe_load
 from argparse import ArgumentParser, FileType
 from pathlib import Path
 from collections import defaultdict
-from term_lookup import pull_details, write_cache, remote_calls
+import cmg_transform.tools
+from cmg_transform.tools.term_lookup import pull_details, write_cache, remote_calls
 
 from ncpi_fhir_plugin.common import CONCEPT, constants
 
-from variant_details import Variant
+from cmg_transform.tools.variant_details import Variant
 
-from cmg_transform import Transform
+from cmg_transform import Transform, InvalidID
 from cmg_transform.discovery_variant import DiscoveryVariant
 from cmg_transform.disease import Disease
 from cmg_transform.patient import Patient
-from cmg_transform.sample import Sample
 from cmg_transform.sequencing import Sequencing 
+from cmg_transform.consent import ConsentGroup
+from cmg_transform.specimen import Specimen
 
 from cmg_transform.change_logger import ChangeLog
+
+import pdb
 
 # Theoretically, there will be a sample_provider and a sample_source coming from the
 # data. In Terra, that doesn't always seem true, but I suspect that those assumed based
@@ -32,171 +38,192 @@ def Run(output, study_name, dataset, delim=None):
     else:
         delim = dataset['delim']
 
-    # We need a way to point back to the family when we parse our specimen file
-    family_lkup = {}
+    study_title = dataset['study_title']
+    study_id = dataset['study_id']
 
-    # There is currently no reference to the proband from parent rows, so we 
-    # need to define that. 
-    proband_relationships = defaultdict(dict)           # parent_id => "relationship" => proband_id 
+    study_group = ConsentGroup(study_name, study_title=study_title, study_id=study_id, group_name=f"{study_name}-complete", consent_name=None)
 
-    if 'field_map' in dataset:
-        Transform.LoadFieldMap(dataset['field_map'])
-        print(Transform._field_map)
+    # We'll dump consents for each group as they are parsed then the entire study
+    fconsent = open(output /"consent_groups.tsv", 'wt')
+    wconsent = csv.writer(fconsent, delimiter='\t', quotechar='"')
+    ConsentGroup.write_default_header(wconsent)
 
-    if 'data_map' in dataset:
-        Transform.LoadDataMap(dataset['data_map'])
-        print(Transform._data_map)
-        print(Transform._data_transform)
+    with open(output / "subject.tsv", 'wt') as subf, \
+            open(output / "disease.tsv", 'wt') as disf, \
+            open(output / "hpo.tsv", 'wt') as hpof, \
+            open(output / "specimen.tsv", 'wt') as specf, \
+            open(output / "sequencing.tsv", 'wt') as seqf, \
+            open(output / "discovery_variant.tsv", 'wt') as discf, \
+            open(output / "discovery_report.tsv", 'wt') as disrf:
 
-    drs_ids = {}
-    if 'drs' in dataset:
-        with open(dataset['drs'], 'rt') as file:
-            reader = csv.DictReader(file, delimiter='\t', quotechar='"')
+        wsubject = csv.writer(subf, delimiter='\t', quotechar='"')
+        Patient.write_default_header(wsubject)
 
-            for row in reader:
-                locals = dict(zip(row['filenames'].split(","), row['object_id'].split(",")))
-                for fn in locals.keys():
-                    drs_ids[fn] = locals[fn]
+        wdisease = csv.writer(disf, delimiter='\t', quotechar='"')
+        Disease.write_header(wdisease)
 
-    diseases = []
-    with open(dataset['subject'], 'rt', encoding='utf-8-sig') as file:
-        Transform._cur_filename = 'subject'
-        reader = Transform.GetReader(file, delimiter=delim)
+        whpo = csv.writer(hpof, delimiter='\t', quotechar='"')
+        Disease.hpo_write_header(whpo)
 
-        print(f"The Patient: {dataset['subject']}")
-        with open(output / "subject.tsv", 'wt') as outf:
-            writer = csv.writer(outf, delimiter='\t', quotechar='"')
-            Patient.write_default_header(writer)
+        wspecemin = csv.writer(specf, delimiter='\t', quotechar='"')        
+        Specimen.write_default_header(wspecemin)
 
-            peeps = []
-            Transform._linenumber = 1
-            for line in reader:
-                Transform._linenumber += 1
-                #print(f"-- {line}")
-                p = Patient(line, family_lkup, proband_relationships)
-                peeps.append(p)
+        wsequencing = csv.writer(seqf, delimiter='\t', quotechar='"')
+        Sequencing.write_header(wsequencing)
 
-                d = Disease(line, family_lkup)
-                diseases.append(d)
-            # Just in case the pedigree data isn't in order
-            for p in peeps:
-                p.write_default(study_name, writer, proband_relationships)
+        wdisc_var = csv.writer(discf, delimiter='\t', quotechar='"')
+        DiscoveryVariant.writeheader(wdisc_var)
 
-        with open(output / "disease.tsv", 'wt') as outf:
-            writer = csv.writer(outf, delimiter='\t', quotechar='"')
-            Disease.write_header(writer)
+        wdisc_rep = csv.writer(disrf, delimiter='\t', quotechar='"')
+        wdisc_rep.writerow([
+                CONCEPT.STUDY.NAME,
+                CONCEPT.PARTICIPANT.ID,
+                CONCEPT.DISCOVERY.VARIANT.ID
+        ])
+        for consent_name in dataset['consent-groups'].keys():
+            consent = dataset['consent-groups'][consent_name]
+            # We need a way to point back to the family when we parse our specimen file
+            family_lkup = {}
 
+            # There is currently no reference to the proband from parent rows, so we 
+            # need to define that. 
+            proband_relationships = defaultdict(dict)           # parent_id => "relationship" => proband_id 
+
+            if 'field_map' in consent:
+                Transform.LoadFieldMap(consent['field_map'])
+                print(Transform._field_map)
+
+            if 'data_map' in consent:
+                Transform.LoadDataMap(consent['data_map'])
+                print(Transform._data_map)
+                print(Transform._data_transform)
+
+            if 'invalid-ids' in consent:
+                Transform.LoadInvalidIDs(consent['invalid-ids'])
+                print(Transform._invalid_ids)
+
+            consent_group = ConsentGroup(study_name, study_title=study_title, study_id=study_id, group_name=consent_name, consent_name=consent_name)
+
+            drs_ids = {}
+            if 'drs' in consent:
+                with open(consent['drs'], 'rt') as file:
+                    reader = csv.DictReader(file, delimiter='\t', quotechar='"')
+
+                    for row in reader:
+                        locals = dict(zip(row['filenames'].split(","), row['object_id'].split(",")))
+                        for fn in locals.keys():
+                            drs_ids[fn] = locals[fn]
+
+            diseases = []
+            with open(consent['subject'], 'rt', encoding='utf-8-sig') as file:
+                Transform._cur_filename = 'subject'
+                try:
+                    reader = Transform.GetReader(file, delimiter=delim)
+                except:
+                    pdb.set_trace()
+                    print(f"There was an issue with loading data from the file, {consent['subject']}")
+                    reader = Transform.GetReader(file, delimiter=delim)
+
+                peeps = []
+                Transform._linenumber = 1
+                for line in reader:
+                    Transform._linenumber += 1
+                    p = Patient(line, family_lkup, proband_relationships)
+                    peeps.append(p)
+
+                    d = Disease(line, family_lkup)
+                    diseases.append(d)
+                # Just in case the pedigree data isn't in order
+                for p in peeps:
+                    p.write_default(study_name, wsubject, proband_relationships)
+
+            # Diseases
             for d in diseases:
-                d.writerow(study_name, writer)
-        with open(output / "hpo.tsv", 'wt') as outf:
-            writer = csv.writer(outf, delimiter='\t', quotechar='"')
-            Disease.hpo_write_header(writer)
-
+                d.writerow(study_name, wdisease)
+            
+            # Human Phenotypes
             for d in diseases:
-                d.hpo_writerow(study_name, writer)
+                d.hpo_writerow(study_name, whpo)
 
-    seq_centers = {}        # Capture the sequencing centers to add to 
-                            # our sequencing output
-    subj_id = {}            # There are some datasets where there is no
-                            # subject ID in the sequencing file
-    with open(dataset['sample'], 'rt', encoding='utf-8-sig') as file:
-        Transform._cur_filename = 'sample'
-        reader = Transform.GetReader(file, delimiter=delim)
-
-        with open(output / "specimen.tsv", 'wt') as outf:
-            writer = csv.writer(outf, delimiter='\t', quotechar='"')
-            Sample.write_default_header(writer)
-
-            Transform._linenumber = 1
-            for line in reader:
-                Transform._linenumber += 1
-                # We skip over samples that exist twice--a side effect
-                # of concatting wgs onto the the wes data
-                if line['sample_id'] not in Sample.observed:
-                    s = Sample(line, family_lkup, subj_id)
-                    if s.sample_provider:
-                        seq_centers[s.sample_id] = s.sample_provider
-                    s.write_row(study_name, writer)
-
-    with open(dataset['sequencing'], 'rt', encoding='utf-8-sig') as file:
-        Transform._cur_filename = 'sequencing'
-        reader = Transform.GetReader(file, delimiter=delim)
-        
-        with open(output / "sequencing.tsv", 'wt') as outf:
-            writer = csv.writer(outf, delimiter='\t', quotechar='"')
-            Sequencing.write_header(writer)
-
-            Transform._linenumber = 1
-            for row in reader:
-                Transform._linenumber += 1
-                seq = Sequencing(row, seq_centers, subj_id)
-                seq.write_row(study_name, writer, drs_ids)
-
-    if "discovery" in dataset:
-        with open(dataset['discovery'], 'rt', encoding='utf-8-sig') as file:
-            Transform._cur_filename = 'discovery'
-            reader = Transform.GetReader(file, delimiter=delim)
-
-            # subject_id => [variant_id, ...]
-            variants = defaultdict(list)
-            with open(output / "discovery_variant.tsv", 'wt') as outf:
-                writer = csv.writer(outf, delimiter='\t', quotechar='"')
-                DiscoveryVariant.writeheader(writer)
+            seq_centers = {}        # Capture the sequencing centers to add to 
+                                    # our sequencing output
+            subj_id = {}            # There are some datasets where there is no
+                                    # subject ID in the sequencing file
+            with open(consent['sample'], 'rt', encoding='utf-8-sig') as file:
+                Transform._cur_filename = 'sample'
+                reader = Transform.GetReader(file, file_type='sample', delimiter=delim)
 
                 Transform._linenumber = 1
+                for line in reader:
+                    try:
+                        Transform.CheckForBadIDs(line)
+                        Transform._linenumber += 1
+                        # We skip over samples that exist twice--a side effect
+                        # of concatting wgs onto the the wes data
+                        if line['sample_id'] not in Specimen.observed:
+                            s = Specimen(line, consent_name, family_lkup, subj_id)
+                            if s.sample_provider:
+                                seq_centers[s.sample_id] = s.sample_provider
+                            consent_group.add_patient(s.id, s.sample_provider)
+                            study_group.add_patient(s.id, s.sample_provider, fail_on_seq_center=False)
+                            s.write_row(study_name, wspecemin)
+                    except InvalidID as error:
+                        print(repr(error))
+                consent_group.write_data(wconsent)
+                
+            with open(consent['sequencing'], 'rt', encoding='utf-8-sig') as file:
+                reader = Transform.GetReader(file, file_type='sequencing', delimiter=delim)
+                
+                Transform._linenumber = 1
                 for row in reader:
-                    Transform._linenumber += 1
-                    var = DiscoveryVariant(row)
-                    if var.writerow(writer, study_name):
-                        var.add_variant_ids(variants)
+                    try:
+                        Transform.CheckForBadIDs(row)
+                        Transform._linenumber += 1
+                        seq = Sequencing(row, seq_centers, subj_id)
+                        seq.write_row(study_name, wsequencing, drs_ids)
+                    except InvalidID as error:
+                        print(repr(error))
+            if "discovery" in consent:
+                with open(consent['discovery'], 'rt', encoding='utf-8-sig') as file:
+                    Transform._cur_filename = 'discovery'
+                    reader = Transform.GetReader(file, delimiter=delim)
 
+                    # subject_id => [variant_id, ...]
+                    variants = defaultdict(list)
+                    Transform._linenumber = 1
+                    for row in reader:
+                        Transform._linenumber += 1
+                        var = DiscoveryVariant(row)
+                        if var.writerow(wdisc_var, study_name):
+                            var.add_variant_ids(variants)
 
-            with open(output / "discovery_report.tsv", 'wt') as outf:
-                writer = csv.writer(outf, delimiter='\t', quotechar='"')
-                writer.writerow([
-                        CONCEPT.STUDY.NAME,
-                        CONCEPT.PARTICIPANT.ID,
-                        CONCEPT.DISCOVERY.VARIANT.ID
-                ])
-
-                for id in variants.keys():
-                    writer.writerow([study, id, "::".join(variants[id])])
+                    for id in variants.keys():
+                        wdisc_rep.writerow([study_name, id, "::".join(variants[id])])
+    study_group.write_data(wconsent)
+    fconsent.close()
 
 if __name__ == "__main__":
-    config = DataConfig.config()
-    env_options = config.list_environments()
-    ds_options = config.list_datasets()
-
+    # Some files may end up going in a directory corresponding to the environment
+    hostsfile = Path(getenv("FHIRHOSTS", 'fhir_hosts'))
+    config = safe_load(hostsfile.open("rt"))
 
     parser = ArgumentParser()
-    parser.add_argument("-e", 
-            "--env", 
-            choices=env_options, 
-            default='dev', 
-            help=f"Remote configuration to be used")
     parser.add_argument("-d", 
                 "--dataset", 
-                choices=['ALL'] + ds_options,
-                default=[],
+                type=FileType('rt'),
                 help=f"Dataset config to be used",
                 action='append')
     parser.add_argument("-o", "--out", default='output')
     args = parser.parse_args()
 
-    datasets = args.dataset 
-
-    if 'ALL' in datasets:
-        datasets = ds_options
-        
-    if len(datasets) == 0:
-        datasets.append('FAKE-CMG')
-
-    for study in sorted(datasets):
-        dirname = Path(f"{args.out}/{study}/transformed")
+    for dsfile in sorted(args.dataset):
+        study = safe_load(dsfile)
+        study_name = study['study_name'].replace(' ', '_')
+        dirname = Path(f"{args.out}/{study_name}/transformed")
         dirname.mkdir(parents=True, exist_ok=True)
-        ChangeLog.InitDB(args.out, study, purge_priors=True)
+        ChangeLog.InitDB(args.out, study_name, purge_priors=True)
 
-        Run(dirname, study, config.get_dataset(study))
+        Run(dirname, study_name, study)
 
     # Write the term cache to file since the API can sometimes be unresponsive
     write_cache()
--- scripts/02_cmg_load.py
+++ scripts/02_cmg_load.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python
 
 import sys
+from os import getenv, remove
 import logging
 from pathlib import Path
 from time import sleep
 
-from yaml import load, FullLoader
+from yaml import safe_load
 from kf_lib_data_ingest.common.io import read_df
 from kf_lib_data_ingest.etl.load.load import LoadStage
+from ncpi_fhir_client.fhir_client import FhirClient
 
-from fhir_walk.config import DataConfig 
-from fhir_walk.fhir_host import FhirHost
-import ncpi_fhir_plugin.fhir_plugin as fhir # import SetAuthorization, remote_authorization
+import ncpi_fhir_plugin as fhir # import SetAuthorization, remote_authorization
 
 import pdb
 
 from argparse import ArgumentParser, FileType
 
 if __name__ == "__main__":
+    #   "family_relationship",
     all_loadable_classes = [
         "patient", 
+        "consent",
+        "group",
         "research_study", 
-        "research_subject", 
+        "research_subject",
         "family_relationship",
         "specimen",
         "disease",
@@ -37,9 +40,11 @@ if __name__ == "__main__":
     ]
     """ For now, we'll leave these off. None of those fields mapped well anyway
         "sequencing_file_info"  """
-    config = DataConfig.config()
-    env_options = config.list_environments()
-    ds_options = config.list_datasets()
+
+    hostsfile = Path(getenv("FHIRHOSTS", 'fhir_hosts'))
+    config = safe_load(hostsfile.open("rt"))
+    
+    env_options = config.keys()
     parser = ArgumentParser()
     parser.add_argument("-e", 
                 "--env", 
@@ -48,8 +53,7 @@ if __name__ == "__main__":
                 help=f"Remote configuration to be used")
     parser.add_argument("-d", 
                 "--dataset", 
-                choices=ds_options + ['ALL'],
-                default=[],
+                type=FileType('rt'),
                 help=f"Dataset config to be used",
                 action='append')
     parser.add_argument("-o", "--out", default='output')
@@ -59,44 +63,75 @@ if __name__ == "__main__":
                 default=[], 
                 action='append',
                 help=f"Which module(s) should be loaded? Default is all of them")
+    parser.add_argument("-b", 
+                "--write-bundle",
+                action='store_true',
+                help="Optionally write a bundle file")
+    parser.add_argument("-p",
+                "--purge-ids",
+                action='store_true',
+                help="Purge the ID cache. Use only the database has been rebuilt and it's contents look different from when this was last run.")
     args = parser.parse_args()
 
     list_of_class_names_to_load = args.modules_to_load
     if len(list_of_class_names_to_load) == 0:
         list_of_class_names_to_load = all_loadable_classes
 
-    fhir_host = config.set_host(args.env)
+    #pdb.set_trace()
+    fhir_host = FhirClient(config[args.env])
+    fhir.set_fhir_server(fhir_host)
+    
     datasets = args.dataset 
-    if len(datasets) == 0:
-        datasets.append('FAKE-CMG')
-    elif "ALL" in datasets:
-        datasets = ds_options
 
     path_to_my_target_service_plugin = "ncpi_fhir_plugin/fhir_plugin.py"
     target_service_base_url = fhir_host.target_service_url
  
-    for study_id in sorted(datasets):
+    for dsfile in datasets:
+        study = safe_load(dsfile)
+        study_id = study['study_id']
+        study_name = study['study_name'].replace(' ', '_')
+        study_title = study['study_title']
+
         print(f"Loading '{study_id}'")
-        logging.basicConfig(filename=f'{args.out}/{study_id}-{args.env}-load.log',
-                                filemode='w',
+        log_filename = f"{args.out}/{study_name}-{args.env}-load.log"
+        print(f"Logfile: {log_filename}")
+
+        # This clears out previous logging entities so we can determine what and where to log
+        for handler in logging.root.handlers[:]:
+            logging.root.removeHandler(handler)
+
+        logging.basicConfig(filename=log_filename,
+                                filemode='wt',
                                 format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                                 datefmt='%H:%M:%S',
                                 level=logging.DEBUG)
 
-
-        input_file_dir = f"{args.out}/{study_id}/transformed"
+        logging.info(f"Does this work? We tried to write to a file named {log_filename}")
+        fhir_host.init_log()
+        input_file_dir = f"{args.out}/{study_name}/transformed"
         path_to_cache_storage_directory = Path(f"{input_file_dir}/{args.env}")
         path_to_cache_storage_directory.mkdir(parents=True, exist_ok=True)
 
+        if args.purge_ids:
+            cache_file = f"{path_to_cache_storage_directory}/LoadStage/localhost_8000_{study_id}_uid_cache.db"
+            print(f"Purging local cache: {cache_file}")
+            remove(cache_file)
+
+        if args.write_bundle:
+            fhir_host.init_bundle(f"{args.out}/{study_id}-{args.env}.json", study_id)
+
         specimens = read_df(f'{input_file_dir}/specimen.tsv')
+        consents = read_df(f'{input_file_dir}/consent_groups.tsv')
         subjects = read_df(f'{input_file_dir}/subject.tsv')
         disease = read_df(f'{input_file_dir}/disease.tsv')
         hpo = read_df(f'{input_file_dir}/hpo.tsv')
         sequencing_data = read_df(f'{input_file_dir}/sequencing.tsv')
-
+        
         basic_reports = {
             "default": subjects,
-            'research_study': specimens,
+            'research_study': consents,
+            'consent': consents,
+            'group': consents,
             'research_subject': specimens,
             'tissue_affected_status': specimens,
             'sequencing_center': specimens,
@@ -121,7 +156,7 @@ if __name__ == "__main__":
             discovery_report = read_df(f"{input_file_dir}/discovery_report.tsv")
 
             basic_reports["discovery_report"] = discovery_report
-        #pdb.set_trace()
+
         outcome = LoadStage(
             path_to_my_target_service_plugin,
             target_service_base_url,
@@ -130,3 +165,4 @@ if __name__ == "__main__":
             str(path_to_cache_storage_directory)
         ).run(basic_reports)
 
+        fhir_host.close_bundle()
